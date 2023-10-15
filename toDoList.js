e34eae463f902731a22a1f2c70f7e3679214498d
DrishtiT <tanwardrishti20@gmail.com>
2023-10-15 20:41:36 +0530

    modified

--- toDoList.js
+++ toDoList.js
@@ -1,16 +1,17 @@
 
+// when task buttons are clicked.
+
 
 let windowWidth = window.innerWidth;
 
 
+document.addEventListener('DOMContentLoaded', () => {
 
-document.addEventListener('DOMContentLoaded', ()=> {
-
-   let activeSectionId = localStorage.getItem('activeSectionId');
-   if(activeSectionId){
-   let activeId = document.getElementById(activeSectionId);
-   activeId.classList.add('active');}
-
+    let activeSectionId = localStorage.getItem('activeSectionId');
+    if (activeSectionId) {
+        let activeId = document.getElementById(activeSectionId);
+        activeId.classList.add('active');
+    }
 
 })
 
@@ -26,9 +27,9 @@ whichTasks.forEach(task => {
 
             let activeSectionId = localStorage.getItem('activeSectionId');
 
-            if(activeSectionId){
-            let activeId = document.getElementById(activeSectionId);
-            activeId.classList.remove('active');
+            if (activeSectionId) {
+                let activeId = document.getElementById(activeSectionId);
+                activeId.classList.remove('active');
             }
 
             const clickedElement = Event.currentTarget;
@@ -36,41 +37,59 @@ whichTasks.forEach(task => {
 
             const sections = document.querySelectorAll('.section');
             sections.forEach(section => {
-                
+
 
                 section.classList.remove('active');
             })
-            
+
 
             const targetSection = document.getElementById(targetId);
-            targetSection.classList.add('active'); 
+            targetSection.classList.add('active');
 
-            localStorage.setItem('activeSectionId' , targetId);
-
-            
-
-            
-        }
-        else if (windowWidth <=699){
-
-
-         const sectionOne = document.querySelector(".sectionOne");
-          sectionOne.classList.add('displayOff');
-        
-          const clickedButton = Event.currentTarget;
-          const button = clickedButton.getAttribute('data-target');
-
-          const Id = document.getElementById(button);
-          console.log(Id);
-          Id.classList.add('displayOn'); 
-
+            localStorage.setItem('activeSectionId', targetId);
 
 
 
 
         }
 
+        else if (windowWidth <= 699) {
 
+
+            const sectionOne = document.querySelector(".sectionOne");
+            sectionOne.classList.add('displayOff');
+
+            const clickedButton = Event.currentTarget;
+            const button = clickedButton.getAttribute('data-target');
+
+            const Id = document.getElementById(button);
+            console.log(Id);
+            Id.classList.add('displayOn');
+
+
+
+            // when arrow or backButton is clicked.
+
+
+            const backButton = document.querySelector(".backButton");
+            console.log(backButton);
+
+            backButton.addEventListener('click', () => {
+
+                Id.classList.remove('displayOn');
+                Id.classList.add('displayOff');
+
+                sectionOne.classList.remove('displayOff');
+
+
+
+                // const section = document.querySelectorAll(".section");
+                // section.classList.add('displayOff');
+
+
+            });
+
+        }
 
     });
 });
@@ -78,40 +97,37 @@ whichTasks.forEach(task => {
 window.addEventListener('resize', () => {
     windowWidth = window.innerWidth;
     console.log(windowWidth);
-    console.log("hello");
-
-
 
 });
 
+
+
+
+// when add task is clicked.
 
 const addTask = document.getElementsByClassName('addTaskLogo')[0];
 const prompt = document.getElementsByClassName('prompt')[0];
 const close = document.getElementsByClassName('close')[0];
 console.log(addTask);
 
-addTask.addEventListener('click', ()=>{
+addTask.addEventListener('click', () => {
 
-prompt.style.display ='block';      
-      
+    prompt.style.display = 'block';
+
 });
 
-close.addEventListener('click', ()=> {
-       
-         prompt.style.display = 'none';
+close.addEventListener('click', () => {
+
+    prompt.style.display = 'none';
 
 });
 
 
 let addTaskLogo = document.querySelector(".addTaskLogo ");
 
-// addTaskLogo.addEventListener("mouseenter", () =>{
-
-//     addTaskLogo.style.backgroundColor = "rgba(78, 78, 78, 0.635)"
-// });
 
 
-addTaskLogo.addEventListener("mousedown", () =>{
+addTaskLogo.addEventListener("mousedown", () => {
     addTaskLogo.style.transform = "scale(0.9)";
 
 
@@ -122,4 +138,14 @@ addTaskLogo.addEventListener("mouseup", () => {
     console.log("bye");
 
     addTaskLogo.style.transform = "scale(1)";
-})
+});
+
+
+
+
+
+
+
+
+
+
